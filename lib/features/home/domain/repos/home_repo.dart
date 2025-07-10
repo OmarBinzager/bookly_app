11d7240bafdba3eadb4936293cb5d305add3e864
OmarBinzager <151304161+OmarBinzager@users.noreply.github.com>
2025-07-10 19:05:05 +0300

    add either and failure class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,9 +1,10 @@
 
 
-
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/home_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeatureBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeatureBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
